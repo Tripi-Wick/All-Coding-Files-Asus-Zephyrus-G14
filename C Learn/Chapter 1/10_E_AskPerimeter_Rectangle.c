b9325b41a86ca73cf4e35e31b8b6038b220ddad1
Tripi-Wick <kimmu1370@gmail.com>
2022-02-10 12:40:52 +0530

    Random

---
+++ C Learn/Chapter 1/10_E_AskPerimeter_Rectangle.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+int main(){
+
+    int L,B,P;
+
+    printf("Type the Length of Rectangle "); scanf("%d",&L);
+    printf("Type the Breadth of Rectangle "); scanf("%d",&B);
+    printf("So the Preimeter of Rectangle is %d",2*(L+B));
+
+    return 0;
+
+}
