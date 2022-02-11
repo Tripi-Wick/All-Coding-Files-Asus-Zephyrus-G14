22e4c2086371adf5cf40be6913cdd3f9272c0509
Tripi-Wick <kimmu1370@gmail.com>
2022-02-12 03:26:57 +0530

    new code yaa...

--- C Learn/Chapter 3/2_Driving_License.c
+++ C Learn/Chapter 3/2_Driving_License.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+int main()
+{
+    int a;
+
+    printf("Enter your age "); scanf("%d",&a);
+
+    if(a<70 && a>18)
+    {
+        printf("You are eligible for driving license");
+    }
+
+    else
+    {
+        printf("You are not eligible for driving");
+    }
+
+    return 0;
+}
+
