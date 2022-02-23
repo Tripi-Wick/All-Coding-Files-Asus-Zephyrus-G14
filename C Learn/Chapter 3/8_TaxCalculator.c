90b00708d8e34b94c93107e8cf8dc33e5f5f3f3b
Tripi-Wick <kimmu1370@gmail.com>
2022-02-23 23:44:30 +0530

    finally done

--- C Learn/Chapter 3/8_TaxCalculator.c
+++ C Learn/Chapter 3/8_TaxCalculator.c
@@ -3,28 +3,28 @@
 int main()
 {
     float I;
-    printf("Enter Your Income "); scanf("%f",I);
+    printf("Enter Your Income "); scanf("%f",&I);
     
     if(0<=I && I<=250000)
         {
             printf("You have to pay no tax ");
         }
     
-    else if(250000<I && I<=500000)
+    else if(250000<=I && I<=500000)
         {
-            float A = (5/100)*I ;
+            float A = 0.05*I ;
             printf("You will pay 5 percent tax which is equal to %f \n", A );
         }
 
     else if(500000<I && I<=1000000)
         {
-            float B = (20/100)*I ;
-            printf("You will pay 20 percent tax which is equal to %f \n", B  );
+            float B = 0.02*I ;
+            printf("You will pay 20 percent tax which is equal to %f \n", B );
         }
 
     else if(1000000<I)
         {
-            float C = (30/100)*I ;
+            float C = 0.03*I ;
             printf("You will pay 30 percent tax which is equal to %f \n", C );
         }
 
