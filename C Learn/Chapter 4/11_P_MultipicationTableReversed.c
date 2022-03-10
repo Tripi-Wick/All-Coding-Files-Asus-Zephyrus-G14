422963ccb3bdfb742d690b6dead5f5a06b9678d7
Tripi-Wick <kimmu1370@gmail.com>
2022-03-11 00:08:11 +0530

    end

---
+++ C Learn/Chapter 4/11_P_MultipicationTableReversed.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter the number whose multipication table is required in reversed order ");
+    scanf("%d",&n);
+
+    int a=10;
+
+    while(a>0){printf("%d*%d=%d\n",n,a,n*a);a--;}
+
+    return 0;
+}
---
+++ C Learn/Chapter 4/12_P_SumWithLoop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,a=1;
+
+    printf("Enter the numbers whose Sum You wanna see "); scanf("%d",&n);
+
+    // 1:- Sum of n natural numbers by While loop:-
+    // (Its sort of Wrong)
+    // 
+    // while(a<=n) 
+    // {
+    //     printf("%d\n",a*(a+1)/2); 
+    //     a++;
+    // }
+
+    return 0;
+}
