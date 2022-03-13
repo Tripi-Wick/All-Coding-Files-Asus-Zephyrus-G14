ac8f559074f56b8f75ae3e8f63e728fb7812fe0a
Tripi-Wick <kimmu1370@gmail.com>
2022-03-14 00:11:25 +0530

    fucking done finally

--- C Learn/Chapter 4/12_P_SumWithLoop.c
+++ C Learn/Chapter 4/12_P_SumWithLoop.c
@@ -2,13 +2,14 @@
 
 int main()
 {
-    int n,a=1;
+    int n,a=1,sum=0;
 
     printf("Enter the numbers whose Sum You wanna see "); scanf("%d",&n);
 
+
     // 1:- Sum of n natural numbers by While loop:-
-    // (Its sort of Wrong)
-    //  
+    //(Its sort of Wrong)
+     
     // while(a<=n) 
     // {
     //     printf("%d\n",a*(a+1)/2); 
@@ -42,6 +43,28 @@ int main()
     // 
     // printf("The value of %d natural numbers is: %d\n",n,(a*(a+1)/2));
 
+
+
+    // 4th:- By Harry
+    // 
+    // int i=1, sum=0, n=10;
+    // // for(i=1; i<=n; i++){
+    // //     sum +=i;
+    // // }
+    // while( i<=n){
+    //     sum +=i;
+    //     i++; 
+    // }
+    // printf("The value of sum(1 to 10) is %d", sum);
+
+
+    // 5th:- The thing I fucking wanted to make from that long finally after two days 🥲
+
+    for(a=1; a<=n; a++)
+    {
+    printf("%d+%d",sum,a);
+    printf("=%d\n",sum+=a);
+    }
     return 0;
 
 }
---
+++ C Learn/Chapter 4/13_P_8Multiple_Sum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int product=0, n;
+
+    for(n=1; n<11; n++)
+    // {
+    //     product+=8*n;
+    // }
+
+    // The thing I fucking Wanted to Create rom two days 
+    // {
+    //     printf("%d+%d",product,8*n);
+    //     printf("=%d\n",product+=8*n);
+    // }
+
+    printf("the value of sum of table of 8 is %d ",product);
+
+    
+
+    return 0;
+}
