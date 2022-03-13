046bbf795b30d96f0f644010c936875001f5a191
Tripi-Wick <kimmu1370@gmail.com>
2022-03-13 23:14:08 +0530

    done sir

--- C Learn/Chapter 4/12_P_SumWithLoop.c
+++ C Learn/Chapter 4/12_P_SumWithLoop.c
@@ -8,12 +8,40 @@ int main()
 
     // 1:- Sum of n natural numbers by While loop:-
     // (Its sort of Wrong)
-    // 
+    //  
     // while(a<=n) 
     // {
     //     printf("%d\n",a*(a+1)/2); 
     //     a++;
     // }
 
+
+
+    //2:- Sum of n natural numbers by while loop:-
+    // while(a<n)
+    // {
+    //     a++;
+    // }
+    // 
+    // printf("The value of %d natural numbers is: %d\n",n,(a*(a+1)/2));
+
+
+
+    //3:- Sum of n matural numbers by do while loop:-
+    // 
+    // do{a++;}
+    // while(a<n);
+    //
+    // printf("The value of %d natural numbers is: %d\n",n,(a*(a+1)/2));
+
+    
+
+    //3:- Sum of n natural numbers by for loop:-
+    // 
+    // for( ; a<n; a++){}
+    // 
+    // printf("The value of %d natural numbers is: %d\n",n,(a*(a+1)/2));
+
     return 0;
+
 }
